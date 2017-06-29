036f9f277d924f592bf54a6d8818a3e75ac5803e
trujunzhang <trujunzhang@gmail.com>
2017-06-30 07:16:42 +0800

    IEATTA web app.

--- src/components/lib/postsoverlay/page/PostsSingleHeader.jsx
+++ src/components/lib/postsoverlay/page/PostsSingleHeader.jsx
@@ -16,13 +16,12 @@ class PostsSingleHeader extends Component {
             My Two Cents
           </h1>
           <div className="u-nowrap claim-status_teaser js-claim-status-hover">
-        <span aria-hidden="true" style="width: 18px; height: 18px;"
+        <span aria-hidden="true" id="icon_18X18"
               className="icon icon--18-checkmark-badged icon--size-18 icon--blue-dark claim-status_icon u-space-r1 claim-status_icon--claimed">
-    <svg className="icon_svg">
-    </svg>
-</span>Claimed
+          <svg className="icon_svg"></svg>
+        </span>
+            Claimed
           </div>
-
 
           <div className="u-hidden js-claim-status-hover-content">
             <p className="u-space-b0">
@@ -49,19 +48,19 @@ class PostsSingleHeader extends Component {
 
 
               <span className="review-count rating-qualifier">
-            130 reviews
-    </span>
+                130 reviews
+              </span>
 
             </div>
 
 
             <div className="rating-details">
-              <a href="javascript:;" className="chiclet-link chiclet-link--with-text show-tooltip js-rating-details">
-            <span aria-hidden="true" style="width: 14px; height: 14px;"
+              <a className="chiclet-link chiclet-link--with-text show-tooltip js-rating-details">
+            <span aria-hidden="true" id="icon_14X14"
                   className="icon icon--14-histogram icon--size-14 icon--currentColor">
-    <svg className="icon_svg">
-    </svg>
-</span> Details
+              <svg className="icon_svg"></svg>
+            </span>
+                Details
                 <span className="offscreen">, Opens a popup</span>
 
                 <span className="tooltip-wrapper">
@@ -75,28 +74,27 @@ class PostsSingleHeader extends Component {
           <div className="price-category">
                     <span className="bullet-after">
 
-        <span className="business-attribute price-range">$$</span>
-        </span>
+                      <span className="business-attribute price-range">$$</span>
+                    </span>
             <span className="category-str-list">
                     <a href="/c/la/southern">Southern</a>,
                     <a href="/c/la/seafood">Seafood</a>,
                     <a href="/c/la/soulfood">Soul Food</a>
-    </span>
-
+            </span>
 
             <a className="edit-category chiclet-link chiclet-link--with-text show-tooltip"
                data-pop-uri="/edit_category_popup/30jrTz8vh1xSXdtXMvt-mA" data-ro-mode-action="edit categories"
                href="/biz_attribute?biz_id=30jrTz8vh1xSXdtXMvt-mA" data-component-bound="true">
-        <span aria-hidden="true" style="width: 14px; height: 14px;"
-              className="icon icon--14-pencil icon--size-14 icon--currentColor">
-    <svg className="icon_svg">
-    </svg>
-</span> Edit
+              <span aria-hidden="true" id="icon_14X14"
+                    className="icon icon--14-pencil icon--size-14 icon--currentColor">
+                <svg className="icon_svg"></svg>
+              </span>
+              Edit
               <span className="offscreen">, Opens a popup</span>
 
               <span className="tooltip-wrapper">
-            <span className="tooltip">Edit categories</span>
-        </span>
+                <span className="tooltip">Edit categories</span>
+              </span>
             </a>
 
           </div>
@@ -112,54 +110,52 @@ class PostsSingleHeader extends Component {
         <div className="biz-page-actions nowrap">
           <a className="ybtn ybtn--primary war-button"
              href="/writeareview/biz/30jrTz8vh1xSXdtXMvt-mA?return_url=%2Fbiz%2F30jrTz8vh1xSXdtXMvt-mA">
-            <span aria-hidden="true" style="width: 24px; height: 24px;"
+            <span aria-hidden="true" id="icon_24X24"
                   className="icon icon--24-star icon--size-24 icon--currentColor u-space-r-half icon--fallback-inverted">
-    <svg className="icon_svg"></svg>
-</span>Write a Review
+              <svg className="icon_svg"></svg>
+            </span>
+            Write a Review
           </a>
           <span className="ybtn-group clearfix">
 
-    <a className="ybtn ybtn--small add-photo-button" href="/biz_user_photos/30jrTz8vh1xSXdtXMvt-mA/upload">
-            <span aria-hidden="true" style="width: 18px; height: 18px;"
-                  className="icon icon--18-add-photo icon--size-18 icon--currentColor">
-    <svg className="icon_svg"></svg>
-</span>
-            Add Photo
-    </a>
+            <a className="ybtn ybtn--small add-photo-button" href="/biz_user_photos/30jrTz8vh1xSXdtXMvt-mA/upload">
+              <span aria-hidden="true" id="icon_18X18"
+                    className="icon icon--18-add-photo icon--size-18 icon--currentColor">
+                <svg className="icon_svg"></svg>
+              </span>
+              Add Photo
+            </a>
 
+            <a className="ybtn ybtn--small share-icon js-business-send-to-friend"
+               data-pop-uri="/send_to_friend/business/30jrTz8vh1xSXdtXMvt-mA" data-ro-mode-action="share a business"
+               href="/biz_share/30jrTz8vh1xSXdtXMvt-mA" data-component-bound="true">
+              <span aria-hidden="true" id="icon_18X18"
+                    className="icon icon--18-share icon--size-18 icon--currentColor">
+                <svg className="icon_svg"></svg>
+              </span>
+              <span className="js-popup-link-text">Share</span>
+              <span className="offscreen">, Opens a popup</span>
+            </a>
 
-
-
-
-    <a className="ybtn ybtn--small share-icon js-business-send-to-friend"
-       data-pop-uri="/send_to_friend/business/30jrTz8vh1xSXdtXMvt-mA" data-ro-mode-action="share a business"
-       href="/biz_share/30jrTz8vh1xSXdtXMvt-mA" data-component-bound="true">
-            <span aria-hidden="true" style="width: 18px; height: 18px;"
-                  className="icon icon--18-share icon--size-18 icon--currentColor">
-    <svg className="icon_svg"></svg>
-</span>
-            <span className="js-popup-link-text">Share</span>
-            <span className="offscreen">, Opens a popup</span>
-    </a>
-
-    <a className="ybtn ybtn--small bookmark-button js-action-bar-bookmark-button not-bookmarked"
-       data-bookmark-button-position="action-bar" data-ro-mode-action="add a bookmark"
-       data-signup-object="biz_id:30jrTz8vh1xSXdtXMvt-mA" href="/signup?return_url=%2Fbiz%2F30jrTz8vh1xSXdtXMvt-mA"
-       data-component-bound="true">
-            <span aria-hidden="true" style="width: 18px; height: 18px;"
-                  className="icon icon--18-bookmark icon--size-18 icon--currentColor">
-    <svg className="icon_svg"></svg>
-</span>
+            <a className="ybtn ybtn--small bookmark-button js-action-bar-bookmark-button not-bookmarked"
+               data-bookmark-button-position="action-bar" data-ro-mode-action="add a bookmark"
+               data-signup-object="biz_id:30jrTz8vh1xSXdtXMvt-mA"
+               href="/signup?return_url=%2Fbiz%2F30jrTz8vh1xSXdtXMvt-mA"
+               data-component-bound="true">
+              <span aria-hidden="true" id="icon_18X18"
+                    className="icon icon--18-bookmark icon--size-18 icon--currentColor">
+                <svg className="icon_svg"></svg>
+              </span>
             <span className="js-popup-link-text">Bookmark</span>
             <span className="offscreen">, Opens a popup</span>
-    </a>
+            </a>
 
-    </span>
+          </span>
         </div>
 
         <small className="u-absolute u-sticky-right">
-            <span className="js-edit-bookmark-link u-pseudo-link" data-has-note="false">
-    </span>
+          <span className="js-edit-bookmark-link u-pseudo-link" data-has-note="false">
+          </span>
 
         </small>
 
@@ -337,10 +333,10 @@ class PostsSingleHeader extends Component {
 
 
                 <a className="see-more show-all-overlay" href="/biz_photos/my-two-cents-los-angeles-3">
-        <span aria-hidden="true" style="width: 24px; height: 24px;"
-              className="icon icon--24-grid icon--size-24 icon--inverse icon--fallback-inverted show-all-overlay_icon">
-          <svg className="icon_svg"></svg>
-        </span>
+                  <span aria-hidden="true" id="icon_24X24"
+                        className="icon icon--24-grid icon--size-24 icon--inverse icon--fallback-inverted show-all-overlay_icon">
+                    <svg className="icon_svg"></svg>
+                  </span>
                   See all 124
                 </a>
 
