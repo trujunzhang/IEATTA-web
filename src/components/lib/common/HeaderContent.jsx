80256f58eb4a49d4e9470b2ac98af66322bc2353
trujunzhang <trujunzhang@gmail.com>
2017-06-30 12:09:04 +0800

    IEATTA web app.

--- src/components/lib/common/HeaderContent.jsx
+++ src/components/lib/common/HeaderContent.jsx
@@ -55,10 +55,10 @@ class HeaderContent extends Component {
 
               <div id="topbar-account-item"
                    className="user-account clearfix drop-menu-origin hidden-non-responsive-block responsive-visible-small-block"
-                   data-component-bound="true">
+              >
 
                 <a className="ybtn ybtn--primary drop-menu-link user-account_button"
-                   id="topbar-account-link" data-component-bound="true">
+                   id="topbar-account-link">
                   <span aria-hidden="true" id="icon_14X14"
                         className="icon icon--14-triangle-down icon--size-14 icon--inverse icon--fallback-inverted u-triangle-direction-down user-account_button-arrow responsive-visible-large-inline-block">
                     <svg className="icon_svg"></svg>
@@ -72,7 +72,7 @@ class HeaderContent extends Component {
                     <svg className="icon_svg"></svg>
                   </span>
                 </a>
-                <div id="topbar-account-wrap" className="drop-menu drop-menu-has-arrow" data-component-bound="true">
+                <div id="topbar-account-wrap" className="drop-menu drop-menu-has-arrow">
                   <div className="drop-menu-arrow responsive-hidden-small"></div>
 
                   <div className="arrange arrange--6 arrange--equal drop-menu_auth-buttons">
@@ -143,19 +143,19 @@ class HeaderContent extends Component {
           <label className="main-search_pseudo-input main-search_pseudo-input--find pseudo-input">
             <span className="pseudo-input_text">Find</span>
             <span className="pseudo-input_field-holder"
-                  data-component-bound="true">
+            >
                 <input disabled="" autoComplete="off" spellCheck="false" tabIndex="-1"
                        className="header_content_middle_input"/>
                   <input maxLength="64" id="find_desc" autoComplete="off" value=""
                          placeholder="pizza, pub, Mustafa" className="main-search_field pseudo-input_field"
-                         aria-autoComplete="list" tabIndex="1" data-component-bound="true"/>
-                    <input type="hidden" maxLength="64" data-component-bound="true" name="find_desc" value=""/>
+                         tabIndex="1"/>
+                    <input type="hidden" maxLength="64" name="find_desc" value=""/>
                         </span>
           </label>
           <div
             className="main-search_suggestions suggestions-list-container search-suggestions-list-container hidden"
-            data-component-bound="true">
-            <ul className="suggestions-list" role="listbox" aria-label="Search results"></ul>
+          >
+            <ul className="suggestions-list" role="listbox"></ul>
           </div>
         </div>
 
@@ -173,16 +173,15 @@ class HeaderContent extends Component {
             <span className="main-search_field-holder pseudo-input_field-holder">
                           <input maxLength="80" id="dropperText_Mast" autoComplete="off" value="Los Angeles, CA"
                                  placeholder="Address, Neighbourhood, or Postcode" data-query="Los Angeles, CA"
-                                 className="main-search_field pseudo-input_field" aria-autoComplete="list" tabIndex="2"
-                                 data-component-bound="true"/>
-                          <input type="hidden" maxLength="80" data-component-bound="true"
+                                 className="main-search_field pseudo-input_field" tabIndex="2"/>
+                          <input type="hidden" maxLength="80"
                                  name="find_loc" value="Los Angeles, CA"/>
                         </span>
           </label>
           <div
             className="main-search_suggestions suggestions-list-container location-suggestions-list-container hidden"
-            data-component-bound="true">
-            <ul className="suggestions-list" role="listbox" aria-label="Search results"></ul>
+          >
+            <ul className="suggestions-list" role="listbox"></ul>
           </div>
         </div>
 
@@ -196,7 +195,7 @@ class HeaderContent extends Component {
         className="arrange_unit arrange_unit--fill align-middle main-header--full_arrange_unit main-header_search-container">
         <div className="main-header_search responsive-hidden-medium-only">
           <form method="get" action="/search" id="header_find_form" className="main-search yform u-space-b0"
-                role="search" data-component-bound="true">
+                role="search">
             <div className="arrange arrange--stack">
               <div className="arrange_unit arrange_unit--fill">
                 <div className="arrange arrange--equal arrange--stack">
