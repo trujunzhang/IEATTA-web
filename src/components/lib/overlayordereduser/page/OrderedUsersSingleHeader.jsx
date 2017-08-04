34823eccc4266efb1fa3239da67af8f3852d7060
trujunzhang <trujunzhang@gmail.com>
2017-08-04 08:26:12 +0800

    IEATTA web app.

--- src/components/lib/overlayordereduser/page/OrderedUsersSingleHeader.jsx
+++ src/components/lib/overlayordereduser/page/OrderedUsersSingleHeader.jsx
@@ -6,50 +6,23 @@ import Users from '../../../../lib/users'
 
 class OrderedUsersSingleHeader extends Component {
 
-  renderTopTitle() {
-    const {forEvent} = this.props;
 
+  renderLeftUserAvator(){
 
-    return (
-      <div className="arrange arrange--12">
-        <div className="arrange_unit arrange_unit--fill">
-          <h1>{forEvent.displayName || ''}</h1>
-          <div className="event-details_category-links">
-
-            <span className="category-str-list">
-                <a href="/events/fremont-ca-us/browse?c=8">Festivals &amp; Fairs</a>
-            </span>
-
-          </div>
-        </div>
-      </div>
-
-    )
   }
 
+  renderUserProfileContainer() {
+    return (
+      <div className="user-profile_container">
+        {this.renderLeftUserAvator()}
+      </div>
+    )
+  }
 
   render() {
     return (
       <div className="content-container">
-
-        <div className="event-details_header ysection">
-          {this.renderTopTitle()}
-        </div>
-
-
-        <div
-          className="clearfix layout-block layout-a event-details_cards-container top-shelf_overlap column--responsive">
-
-          <div className="column column-alpha column--responsive">
-            {/*<Telescope.components.OrderedUsersSingleHeaderLeftPanel {...this.props}/>*/}
-          </div>
-
-          <div className="column column-beta column--responsive">
-            {/*<Telescope.components.OrderedUsersSingleHeaderRightMap {...this.props}/>*/}
-          </div>
-
-        </div>
-
+        {this.renderUserProfileContainer()}
       </div>
     )
 
