90d3ff2155b387b91004ba9694a60279cbbb1d5d
trujunzhang <trujunzhang@gmail.com>
2017-08-05 10:40:52 +0800

    IEATTA web app.

--- src/components/lib/home/page/IEARestaurantsList.jsx
+++ src/components/lib/home/page/IEARestaurantsList.jsx
@@ -21,7 +21,7 @@ class IEARestaurantsList extends Component {
 
     const showReady = Posts.showReady(results, hasMore, ready, totalCount, limit, firstPagination)
 
-    if (showReady || true) {
+    if (showReady) {
       return (
         <section className='restaurants-list-loading-panel'>
           <Telescope.components.F8LoadingView/>
