387a37aef7950a635718343d1f7cd78851c3d72a
trujunzhang <trujunzhang@gmail.com>
2017-08-08 10:30:47 +0800

    IEATTA web app.

---
+++ tests/googlemap/google-map-test.js
@@ -0,0 +1,30 @@
+
+'use strict'
+
+/**
+ * ## Mocks
+ *
+ * We don't want to use the devices storage, nor actually call
+ * the server
+ *
+ * Need to mock router so the "keys" are available (see src/__mocks__)
+ */
+
+/**
+ * ## Tests
+ *
+ * authActions
+ */
+describe('google map', () => {
+
+  it('parse google results', () => {
+
+    expect(1).toEqual(2)
+
+    // expect(actions.onAuthFormFieldChange(field, value)).toEqual({
+    //   type: ON_AUTH_FORM_FIELD_CHANGE,
+    //   payload: {field: field, value: value}
+    // })
+  })
+
+})
