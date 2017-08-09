693b5e33cc027b202bf1b46755f82b870f81ce0f
trujunzhang <trujunzhang@gmail.com>
2017-08-09 18:28:29 +0800

    IEATTA web app.

--- src/components/lib/overlayrestaurant/IEAPhotosSelectionLayout.jsx
+++ src/components/lib/overlayrestaurant/IEAPhotosSelectionLayout.jsx
@@ -5,19 +5,52 @@ import Users from '../../../lib/users'
 
 class IEAPhotosSelectionLayout extends Component {
 
+  renderCloseButton() {
+    return (
+      <div className="lightbox-close">
+        Close
+        <span className="icon icon--24-close icon--size-24 icon--currentColor u-space-l-half"
+              id="icon_24X24 ">
+        <svg className="icon_svg">
+    <path
+      d="M17.657 19.07L12 13.415 6.343 19.07 4.93 17.658 10.585 12 4.93 6.343 6.342 4.93 12 10.585l5.657-5.657L19.07 6.34 13.416 12l5.657 5.657-1.413 1.414z"/>
+        </svg>
+      </span>
+      </div>
+    )
+  }
+
+  renderContent() {
+    return (
+      <div className="media-lightbox">
+        <div className="media-details js-media-details js-media-details-template" data-component-bound="true">
+          <div className="media-details_container media-details_container--embed media-details_container--with-sidebar">
+            <div className="media-container js-media-container" data-component-bound="true">
+              <div className="media-details-grid">
+
+                {/*{01-left-panel.html}*/}
+
+                {/*{02-right-panel.html}*/}
+
+              </div>
+            </div>
+
+            {/*{03-left-right-navigator.html}*/}
+
+          </div>
+        </div>
+      </div>
+
+    )
+  }
+
   render() {
     return (
-      <div className="biz-country-us">
 
-        <div className="main-content-wrap main-content-wrap--full">
-          <div className="top-shelf">
-            <Telescope.components.RestaurantsSingleHeader {...this.props}/>
-          </div>
-
-          <div id="super-container" className="content-container">
-            <Telescope.components.RestaurantsDetail {...this.props}/>
-          </div>
-
+      <div id="lightbox" className="lightbox is-enabled lightbox--media-details" data-component-bound="true">
+        <div id="lightbox-inner" className="lightbox-inner">
+          {this.renderCloseButton()}
+          {this.renderContent()}
         </div>
       </div>
     )
