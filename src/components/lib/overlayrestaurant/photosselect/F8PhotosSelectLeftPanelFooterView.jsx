417c5229868faec997d02c672b9ec6a66ed4d4e0
trujunzhang <trujunzhang@gmail.com>
2017-08-09 18:22:15 +0800

    IEATTA web app.

---
+++ src/components/lib/overlayrestaurant/photosselect/F8PhotosSelectLeftPanelFooterView.jsx
@@ -0,0 +1,15 @@
+import React, {Component} from 'react';
+
+class F8PhotosSelectLeftPanelFooterView extends Component {
+  render() {
+    const {photos} = this.props;
+
+    return (
+      <div className="media-landing_gallery photos">
+      </div>
+    );
+  }
+}
+
+
+export default F8PhotosSelectLeftPanelFooterView;
---
+++ src/components/lib/overlayrestaurant/photosselect/F8PhotosSelectRightPanel.jsx
@@ -0,0 +1,15 @@
+import React, {Component} from 'react';
+
+class F8PhotosSelectRightPanel extends Component {
+  render() {
+    const {photos} = this.props;
+
+    return (
+      <div className="media-landing_gallery photos">
+      </div>
+    );
+  }
+}
+
+
+export default F8PhotosSelectRightPanel;
