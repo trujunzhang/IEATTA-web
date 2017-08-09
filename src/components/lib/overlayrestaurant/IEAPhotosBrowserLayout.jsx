a40d8b8106226a973a9ff6a506119b94857844a7
trujunzhang <trujunzhang@gmail.com>
2017-08-09 12:02:34 +0800

    IEATTA web app.

---
+++ src/components/lib/overlayrestaurant/IEAPhotosBrowserLayout.jsx
@@ -0,0 +1,27 @@
+import Telescope from '../index'
+import React, {Component} from 'react'
+import Posts from '../../../lib/posts'
+import Users from '../../../lib/users'
+
+class IEAPhotosBrowserLayout extends Component {
+
+  render() {
+    return (
+      <div className="biz-country-us">
+
+        <div className="main-content-wrap main-content-wrap--full">
+          <div className="top-shelf">
+            <Telescope.components.RestaurantsSingleHeader {...this.props}/>
+          </div>
+
+          <div id="super-container" className="content-container">
+            <Telescope.components.RestaurantsDetail {...this.props}/>
+          </div>
+
+        </div>
+      </div>
+    )
+  }
+}
+
+export default IEAPhotosBrowserLayout;
