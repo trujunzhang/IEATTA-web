718d93198af7c5138f29958483206f71e145f5a7
trujunzhang <trujunzhang@gmail.com>
2017-08-10 17:12:21 +0800

    IEATTA web app.

--- src/components/lib/common/F8PageHeaderButtonsSection.jsx
+++ src/components/lib/common/F8PageHeaderButtonsSection.jsx
@@ -21,27 +21,34 @@ class F8PageHeaderButtonsSection extends Component {
   }
 
   renderEditButton() {
+    const {editLink} = this.props;
     return (
-      <a className="edit-category chiclet-link chiclet-link--with-text show-tooltip"
-         style={{float: 'left'}}>
+      <Link to={editLink}
+            className="edit-category chiclet-link chiclet-link--with-text show-tooltip"
+            style={{float: 'left'}}>
         <span id="icon_14X14" className="icon icon--14-pencil icon--size-14 icon--currentColor">
           <svg className="icon_svg">
             <path
               d="M12.95 3.05c0-.512-.195-1.023-.586-1.414a1.996 1.996 0 0 0-2.83 0L8.122 3.05 2.465 8.707 1.05 12.95l4.243-1.414L10.95 5.88l1.414-1.416c.39-.39.586-.902.586-1.414zm-8.197 7.61l-2.122.71.71-2.123 5.49-5.49 1.415 1.415-5.49 5.49z"/>
           </svg>
         </span>
-        Edit
-      </a>
+        {"Edit"}
+      </Link>
     )
   }
 
   render() {
+
+    const {
+      showEdit = false
+    } = this.props;
+
     return (
       <div className="biz-page-actions nowrap">
 
         {this.renderWriteAReview()}
 
-        {this.renderEditButton()}
+        {showEdit ? this.renderEditButton() : null}
 
         <span className="ybtn-group clearfix">
 
