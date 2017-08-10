e0eb767f92153a1baf109648b87f51c87e4f8902
trujunzhang <trujunzhang@gmail.com>
2017-08-10 16:19:35 +0800

    IEATTA web app.

--- src/components/lib/overlayevent/page/EventsSingleHeader.jsx
+++ src/components/lib/overlayevent/page/EventsSingleHeader.jsx
@@ -13,13 +13,6 @@ class EventsSingleHeader extends Component {
       <div className="arrange arrange--12">
         <div className="arrange_unit arrange_unit--fill">
           <h1>{event.displayName}</h1>
-          <div className="event-details_category-links">
-
-            <span className="category-str-list">
-                <a href="/events/fremont-ca-us/browse?c=8">Festivals &amp; Fairs</a>
-            </span>
-
-          </div>
         </div>
       </div>
 
@@ -33,8 +26,9 @@ class EventsSingleHeader extends Component {
 
         <div className="event-details_header ysection">
           {this.renderTopTitle()}
-        </div>
 
+          <Telescope.components.F8PageHeaderButtonsSection {...this.props}/>
+        </div>
 
         <div
           className="clearfix layout-block layout-a event-details_cards-container top-shelf_overlap column--responsive">
