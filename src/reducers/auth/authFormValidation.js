2716991f5928f1c4ca9f11f9c2c54126e00a8e14
trujunzhang <trujunzhang@gmail.com>
2017-08-12 15:17:05 +0800

    IEATTA web app.

--- src/reducers/auth/authFormValidation.js
+++ src/reducers/auth/authFormValidation.js
@@ -24,6 +24,8 @@ const {
  * @param {Object} state - the Redux state object
  */
 export default function formValidation (state) {
+  debugger
+
   switch (state.form.state) {
     /**
      * ### Logout has no fields, so always valid
