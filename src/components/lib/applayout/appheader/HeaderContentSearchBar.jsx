05685068289f216c59d1d4fa4fe35e2d29c1d3eb
trujunzhang <trujunzhang@gmail.com>
2017-08-14 08:51:54 +0800

    IEATTA web app.

--- src/components/lib/applayout/appheader/HeaderContentSearchBar.jsx
+++ src/components/lib/applayout/appheader/HeaderContentSearchBar.jsx
@@ -9,19 +9,16 @@ class HeaderContentSearchBar extends Component {
         <div className="main-search_suggestions-field search-field-container find-decorator">
           <label className="main-search_pseudo-input main-search_pseudo-input--find pseudo-input">
             <span className="pseudo-input_text">Find</span>
-            <span className="pseudo-input_field-holder"
-            >
-                <input disabled="" autoComplete="off" spellCheck="false" tabIndex="-1"
-                       className="header_content_middle_input"/>
-                  <input maxLength="64" id="find_desc" autoComplete="off" value=""
-                         placeholder="pizza, pub, Mustafa" className="main-search_field pseudo-input_field"
-                         tabIndex="1"/>
-                    <input type="hidden" maxLength="64" name="find_desc" value=""/>
-                        </span>
+            <span className="pseudo-input_field-holder">
+                  <input maxLength="64"
+                         id="find_desc"
+                         autoComplete="off"
+                         value=""
+                         placeholder="pizza, pub, Mustafa"
+                         className="main-search_field pseudo-input_field"
+                  />
+            </span>
           </label>
-          <div
-            className="main-search_suggestions suggestions-list-container search-suggestions-list-container hidden">
-          </div>
         </div>
 
       </div>
@@ -39,50 +36,31 @@ class HeaderContentSearchBar extends Component {
                           <input maxLength="80" id="dropperText_Mast" autoComplete="off" value="Los Angeles, CA"
                                  placeholder="Address, Neighbourhood, or Postcode" data-query="Los Angeles, CA"
                                  className="main-search_field pseudo-input_field" tabIndex="2"/>
-                          <input type="hidden" maxLength="80"
-                                 name="find_loc" value="Los Angeles, CA"/>
                         </span>
           </label>
-          <div
-            className="main-search_suggestions suggestions-list-container location-suggestions-list-container hidden">
-          </div>
         </div>
 
       </div>
     )
   }
 
-  render() {
+  renderSearchIcon() {
     return (
-      <div
-        className="arrange_unit arrange_unit--fill align-middle main-header--full_arrange_unit main-header_search-container">
-        <div className="main-header_search responsive-hidden-medium-only">
-
-          <form method="get" action="/search" id="header_find_form" className="main-search yform u-space-b0"
-                role="search">
-            <div className="arrange arrange--stack">
-              <div className="arrange_unit arrange_unit--fill">
-                <div className="arrange arrange--equal arrange--stack">
-
-                  {this.renderLeftSearchbar()}
-
-                  {/*{this.renderRightSearchbar()}*/}
-                </div>
-              </div>
-
-              <div className="arrange_unit main-search_actions arrange_unit--stack-12">
-                <div className="arrange arrange--wrap arrange--6">
-                  <div
-                    className="arrange_unit hidden-non-responsive-inline-block responsive-visible-small-inline-block main-search_action">
-
-                    <a className="ybtn ybtn--primary main-header_button js-search-close main-search_close">
-                      Cancel
-                    </a>
-                  </div>
-                  <div className="arrange_unit main-search_action arrange_unit--stack-12">
-
-                    <button className="ybtn ybtn--primary main-search_submit main-header_button"
-                            id="header-search-submit" tabIndex="3" title="Search" type="submit" value="submit">
+      <div className="arrange_unit main-search_actions arrange_unit--stack-12">
+        <div className="arrange arrange--wrap arrange--6">
+          <div
+            className="arrange_unit hidden-non-responsive-inline-block responsive-visible-small-inline-block main-search_action">
+            <a className="ybtn ybtn--primary main-header_button js-search-close main-search_close">
+              Cancel
+            </a>
+          </div>
+          <div className="arrange_unit main-search_action arrange_unit--stack-12">
+            <button className="ybtn ybtn--primary main-search_submit main-header_button"
+                    id="header-search-submit"
+                    tabIndex="3"
+                    title="Search"
+                    type="submit"
+                    value="submit">
                             <span className="main-search_action-icon-wrap js-search-icon-wrap">
                                 <span id="icon_24X24"
                                       className="icon icon--24-search icon--size-24 icon--inverse icon--fallback-inverted">
@@ -93,11 +71,33 @@ class HeaderContentSearchBar extends Component {
                                 </span>
                                 <span className="u-offscreen">Search</span>
                             </span>
-                    </button>
-                  </div>
+            </button>
+          </div>
+        </div>
+      </div>
+
+    )
+  }
+
+  render() {
+    return (
+      <div
+        className="arrange_unit arrange_unit--fill align-middle main-header--full_arrange_unit main-header_search-container">
+        <div className="main-header_search responsive-hidden-medium-only">
+
+          <form method="get"
+                id="header_find_form"
+                className="main-search yform u-space-b0"
+                role="search">
+            <div className="arrange arrange--stack">
+              <div className="arrange_unit arrange_unit--fill">
+                <div className="arrange arrange--equal arrange--stack">
+
+                  {this.renderLeftSearchbar()}
                 </div>
               </div>
 
+              {this.renderSearchIcon()}
             </div>
           </form>
 
