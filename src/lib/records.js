e6335df7795ac6f1efffd84c98c713887ad59593
trujunzhang <trujunzhang@gmail.com>
2017-08-17 19:48:47 +0800

    IEATTA.

---
+++ src/lib/records.js
@@ -0,0 +1,41 @@
+/**
+ * The states were interested in
+ */
+const {
+  PARSE_RESTAURANTS,
+  PARSE_USERS,
+  PARSE_RECORDS,
+  PARSE_EVENTS,
+  PARSE_RECIPES,
+  PARSE_PHOTOS,
+  PARSE_REVIEWS,
+  PARSE_PEOPLE_IN_EVENTS
+} = require('./constants').default
+
+const moment = require('moment')
+
+const Records = {}
+
+Records.realmObjects = {
+  'record': {objectSchemaName: PARSE_RECORDS},
+  'restaurant': {objectSchemaName: PARSE_RESTAURANTS},
+  'event': {objectSchemaName: PARSE_EVENTS},
+  'peopleInEvent': {objectSchemaName: PARSE_PEOPLE_IN_EVENTS},
+  'user': {objectSchemaName: PARSE_USERS},
+  'recipe': {objectSchemaName: PARSE_RECIPES},
+  'photo': {objectSchemaName: PARSE_PHOTOS},
+  'review': {objectSchemaName: PARSE_REVIEWS},
+}
+
+Records.realmTypes = {
+  PARSE_RECORDS: 'record',
+  PARSE_RESTAURANTS: 'restaurant',
+  PARSE_EVENTS: 'event',
+  PARSE_PEOPLE_IN_EVENTS: 'peopleInEvent',
+  PARSE_USERS: 'user',
+  PARSE_RECIPES: 'recipe',
+  PARSE_PHOTOS: 'photo',
+  PARSE_REVIEWS: 'review'
+}
+
+export default Records;
