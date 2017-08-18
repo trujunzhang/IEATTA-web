6f4c84b357d91d2301cd917cd964ffc4d39643a5
trujunzhang <trujunzhang@gmail.com>
2017-08-18 18:06:09 +0800

    IEATTA.

--- src/actions/parseRestaurants.js
+++ src/actions/parseRestaurants.js
@@ -54,6 +54,8 @@ const {
 } = require('../lib/constants').default
 
 async function _loadListByType(listTask: Any, objectsQuery: Parse.Query, terms: Any, parseFun: Any, type: Any): Promise<Array<Action>> {
+  // const xxx = await Parse.Cloud.run('statisticUserState', {userId: 'aGkde8iuL6'});
+
   const {
     pageIndex,
     limit,
