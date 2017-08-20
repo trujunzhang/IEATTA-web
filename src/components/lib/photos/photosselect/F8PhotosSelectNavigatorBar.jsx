c238e16d7351ba26fc8f47ae53461c1efa805e7f
trujunzhang <trujunzhang@gmail.com>
2017-08-20 17:57:50 +0800

    IEATTA.

--- src/components/lib/photos/photosselect/F8PhotosSelectNavigatorBar.jsx
+++ src/components/lib/photos/photosselect/F8PhotosSelectNavigatorBar.jsx
@@ -4,8 +4,7 @@ import React, {Component} from 'react';
 class F8PhotosSelectNavigatorBar extends Component {
 
   renderPreIcon() {
-    const {photosListTask, selectPhotoIndex} = this.props;
-    const photos = photosListTask.results;
+    const {selectPhotoIndex} = this.props;
     const havePreIcon = (selectPhotoIndex > 0)
     const preClass = 'media-nav_link media-nav_link--prev js-media-nav_link--prev ' + (havePreIcon ? "" : " is-disabled")
     const linkProps = (havePreIcon) ? {onClick: this.props.onPreIconClick} : {};
@@ -31,11 +30,8 @@ class F8PhotosSelectNavigatorBar extends Component {
   renderNextIcon() {
     const {photosListTask, selectPhotoIndex} = this.props;
     const photos = photosListTask.results;
-
     const haveNextIcon = (selectPhotoIndex < photos.length - 1)
-
     const nextClass = 'media-nav_link media-nav_link--next js-media-nav_link--next ' + (haveNextIcon ? "" : " is-disabled")
-
     const linkProps = (haveNextIcon) ? {onClick: this.props.onNextIconClick} : {};
 
     return (
@@ -46,11 +42,11 @@ class F8PhotosSelectNavigatorBar extends Component {
             <span
               id="icon_48X48"
               className="icon icon--48-chevron-right icon--size-48 icon--inverse icon--fallback-inverted">
-    <svg className="icon_svg">
-    <path
-      d="M18.586 42.008a2.518 2.518 0 0 1-1.614-.588c-1.115-.925-1.296-2.613-.404-3.77L27.098 24l-10.53-13.65c-.892-1.156-.71-2.844.404-3.77 1.116-.924 2.743-.737 3.635.42L32.43 22.325a2.76 2.76 0 0 1 0 3.35L20.606 41.002a2.548 2.548 0 0 1-2.02 1.008z"/>
-    </svg>
-</span>
+            <svg className="icon_svg">
+                <path
+                  d="M18.586 42.008a2.518 2.518 0 0 1-1.614-.588c-1.115-.925-1.296-2.613-.404-3.77L27.098 24l-10.53-13.65c-.892-1.156-.71-2.844.404-3.77 1.116-.924 2.743-.737 3.635.42L32.43 22.325a2.76 2.76 0 0 1 0 3.35L20.606 41.002a2.548 2.548 0 0 1-2.02 1.008z"/>
+            </svg>
+            </span>
       </a>
 
     )
