0d5d6c0d288e6a3e8fa5b1c53c29e54aec6b6b31
trujunzhang <trujunzhang@gmail.com>
2017-08-20 18:18:44 +0800

    IEATTA.

--- src/components/lib/photos/photossingle/F8PhotosSingleTop.jsx
+++ src/components/lib/photos/photossingle/F8PhotosSingleTop.jsx
@@ -21,13 +21,13 @@ class F8PhotosSingleTop extends Component {
                   </Link>
                 </li>
                 <li>
-                                <span
-                                  id="icon_24X24 "
-                                  className="icon icon--24-chevron-right icon--size-24 icon--neutral-gray u-space-r-half">
-    <svg className="icon_svg">
-    <path d="M9.525 5.636L8.11 7.05 13.06 12l-4.95 4.95 1.415 1.414L15.89 12 9.524 5.636z"/>
-    </svg>
-</span>
+
+                  <span id="icon_24X24 "
+                        className="icon icon--24-chevron-right icon--size-24 icon--neutral-gray u-space-r-half">
+                       <svg className="icon_svg">
+                             <path d="M9.525 5.636L8.11 7.05 13.06 12l-4.95 4.95 1.415 1.414L15.89 12 9.524 5.636z"/>
+                       </svg>
+                  </span>
                   {'Photos'}
                 </li>
               </ul>
