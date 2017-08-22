e39d4e312efd6196ed6c50b8aca577cd0031b24c
trujunzhang <trujunzhang@gmail.com>
2017-08-22 12:07:32 +0800

    IEATTA.

--- src/reducers/editModel/editModelReducer.js
+++ src/reducers/editModel/editModelReducer.js
@@ -75,6 +75,11 @@ function editModelReducer(state = initialState, action) {
       let next = setIn(['form', 'fields', 'address'], restaurant.address)
         .setIn(['form', 'fields', 'street_number'], restaurant.street_number)
         .setIn(['form', 'fields', 'route'], restaurant.route)
+        .setIn(['form', 'fields', 'locality'], restaurant.locality)
+        .setIn(['form', 'fields', 'sublocality'], restaurant.sublocality)
+        .setIn(['form', 'fields', 'country'], restaurant.country)
+        .setIn(['form', 'fields', 'postal_code'], restaurant.postal_code)
+        .setIn(['form', 'fields', 'administrative_area'], restaurant.administrative_area)
 
       return next
 
