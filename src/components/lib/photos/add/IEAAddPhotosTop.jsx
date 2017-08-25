f8ad95d07fb8d88a56e74b401913c590ab1bde26
trujunzhang <trujunzhang@gmail.com>
2017-08-25 19:54:59 +0800

    IEATTA.

---
+++ src/components/lib/photos/add/IEAAddPhotosTop.jsx
@@ -0,0 +1,30 @@
+import Telescope from '../index'
+import React, {Component} from 'react'
+
+class IEAAddPhotosTop extends Component {
+
+  constructor(props) {
+    super(props)
+    this.state = this.initialState = {}
+  }
+
+  componentWillReceiveProps(nextProps) {
+    this.setState({})
+  }
+
+  render() {
+    return (
+      <div className="main-content-wrap main-content-wrap--full">
+
+        <div id="super-container" className="content-container">
+
+          <div className="container media-details js-media-details">
+
+          </div>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default IEAAddPhotosTop;
