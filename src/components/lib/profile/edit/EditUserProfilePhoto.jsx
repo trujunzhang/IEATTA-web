07e9867dc4142f318c0522a2c33642973afdd734
trujunzhang <trujunzhang@gmail.com>
2017-08-31 17:39:13 +0800

    IEATTA.

--- src/components/lib/profile/edit/EditUserProfilePhoto.jsx
+++ src/components/lib/profile/edit/EditUserProfilePhoto.jsx
@@ -1,22 +1,38 @@
 import React, {Component} from 'react';
 
+import Photos from '../../../../lib/photos'
+
+import {getLoggedUserMenuLink} from '../../../lib/link'
+import {Link} from 'react-router'
+
 class EditUserProfilePhoto extends Component {
   render() {
+    const {userProfile} = this.props;
+
     return (
       <div className="ysection">
         <h4>Your Profile Photo
           <strong>
+
             <a href="/user_photos?return_url=/profile">
               (Add/Edit)
             </a>
+
           </strong>
         </h4>
 
         <div className="photo-box pb-m">
-          <a className="js-analytics-click" data-analytics-label="user-photo"
+          <a className="js-analytics-click"
              href="/user_photos?return_url=%2Fprofile">
-            <img alt="" className="photo-box-img"
-                 src="https://s3-media1.fl.yelpcdn.com/photo/Hjd0EAdSH-gYJbRBF5nAnw/m.jpg"/>
+
+            <Telescope.components.F8PlaceHolderImage
+              alt={userProfile.username}
+              className="photo-box-img"
+              width="250"
+              height="250"
+              placeholderSource={"/default/user_30_square.png"}
+              source={Photos.getListThumbnailUrl(userProfile)}/>
+
           </a>
         </div>
 
