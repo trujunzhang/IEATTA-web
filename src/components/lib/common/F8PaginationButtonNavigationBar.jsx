5d009525b65333f59be33878d44375a1c8a65b94
trujunzhang <trujunzhang@gmail.com>
2017-09-05 06:03:11 +0800

    Pagination Navigator Bar.

--- src/components/lib/common/F8PaginationButtonNavigationBar.jsx
+++ src/components/lib/common/F8PaginationButtonNavigationBar.jsx
@@ -37,31 +37,73 @@ class F8PaginationButtonNavigationBar extends Component {
       const totalPage = totalCount/limit;
       const currentPage = 1;
 
-    return (
-      <div className="pagination-block">
-        <div className="arrange arrange--stack arrange--baseline arrange--6">
-          <div className="page-of-pages arrange_unit arrange_unit--fill">
-              {`Page ${currentPage} of ${totalPage}`}
-          </div>
+      const arrangeUnits=[];
 
-          <div className="pagination-links arrange_unit">
-            <div className="arrange arrange--baseline">
-
-              <div className="arrange_unit page-option current">
-                <span className="pagination-links_anchor">1</span>
-              </div>
-
+      for(let x = 0; x< totalPage; x++){
+          let row = (
               <div className="arrange_unit page-option">
-                <a className="available-number pagination-links_anchor"
-                   href="/not_recommended_reviews/mourad-restaurant-san-francisco?not_recommended_start=10">
-                  2
-                </a>
+                  <a key={x} className="available-number pagination-links_anchor" onClick={(e)=>this.onPaginationButtonPress(x)}>{x+1}</a>
               </div>
+          )
 
+          if(x === currentPage){
+              row = (
+              <div className="arrange_unit page-option current">
+                  <span key={x} className="pagination-links_anchor">{x+1}</span>
+              </div>
+              )
+          }
+          arrangeUnits.push(row)
+      }
+
+    return (
+        <div className="pagination-block">
+            <div className="arrange arrange--stack arrange--baseline arrange--6">
+                <div className="page-of-pages arrange_unit arrange_unit--fill">
+                    {`Page ${currentPage+1} of ${totalPage}`}
+                </div>
+
+                <div className="pagination-links arrange_unit">
+                    <div className="arrange arrange--baseline">
+
+                        {this.renderPreviousIcon()}
+
+                        {arrangeUnits}
+
+                        {this.renderNextIcon()}
+
+                    </div>
+
+                </div>
+
+            </div>
+        </div>
+    )
+  }
+
+
+    renderPreviousIcon(){
+        return(
+            <div className="arrange_unit">
+                <a className="u-decoration-none block prev pagination-links_anchor" >
+                    <span id="icon_24X24" className="icon icon--24-chevron-left icon--size-24 icon--currentColor">
+                        <svg className="icon_svg">
+                            <path d="M14.475 18.364l1.414-1.414L10.94 12l4.95-4.95-1.415-1.414L8.11 12l6.365 6.364z"/>
+                        </svg>
+                    </span>
+                    <span className="pagination-label responsive-hidden-small">Previous</span>
+                </a>
+            </div>
+        )
+    }
+
+    renderNextIcon(){
+        return(
               <div className="arrange_unit">
-                <a className="u-decoration-none next pagination-links_anchor"
-                   href="/not_recommended_reviews/mourad-restaurant-san-francisco?not_recommended_start=10">
-                  <span className="pagination-label responsive-hidden-small pagination-links_anchor">Next</span>
+                <a className="u-decoration-none next pagination-links_anchor">
+                    <span className="pagination-label responsive-hidden-small pagination-links_anchor">
+                        {"Next"}
+                    </span>
                   <span id="icon_24X24" className="icon icon--24-chevron-right icon--size-24 icon--currentColor">
                        <svg className="icon_svg">
                             <path d="M9.525 5.636L8.11 7.05 13.06 12l-4.95 4.95 1.415 1.414L15.89 12 9.524 5.636z"/>
@@ -69,14 +111,8 @@ class F8PaginationButtonNavigationBar extends Component {
                  </span>
                 </a>
               </div>
-            </div>
-          </div>
-
-        </div>
-      </div>
-    )
-  }
-
+        )
+    }
 
 	render() {
       return this.renderReviewsListFooter();
