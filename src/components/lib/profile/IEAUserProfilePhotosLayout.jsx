148cc0eb6f4681a2d3093731f6b15a4ba3afab87
trujunzhang <trujunzhang@gmail.com>
2017-09-05 12:39:47 +0800

    IEATTA.

--- src/components/lib/profile/IEAUserProfilePhotosLayout.jsx
+++ src/components/lib/profile/IEAUserProfilePhotosLayout.jsx
@@ -106,31 +106,6 @@ class IEAUserProfilePhotosLayout extends Component {
     return (<Telescope.components.F8LoadingView loadingClass="placeholder_1WOC3"/>)
   }
 
-
-  onPreIconClick() {
-    const {photosListTask, selectPhotoIndex} = this.state;
-    const photos = photosListTask.results;
-
-    let preIndex = selectPhotoIndex - 1;
-    if (preIndex < 0) preIndex = 0;
-    this.setState({selectPhotoIndex: preIndex})
-
-    this.props.router.push({pathname: this.props.location.pathname, query: {select: photos[preIndex].id}})
-  }
-
-  onNextIconClick() {
-    const {photosListTask, selectPhotoIndex} = this.state;
-    const photos = photosListTask.results;
-    const totalPhotosLength = photos.length;
-
-    let nextIndex = selectPhotoIndex + 1;
-    if (nextIndex >= totalPhotosLength) nextIndex = totalPhotosLength - 1;
-    this.setState({selectPhotoIndex: nextIndex})
-
-    this.props.router.push({pathname: this.props.location.pathname, query: {select: photos[nextIndex].id}})
-  }
-
-
 }
 
 import {connect} from 'react-redux'
