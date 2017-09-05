97ee972d3ebc457727c4f92dc0157b348cfac4ac
trujunzhang <trujunzhang@gmail.com>
2017-09-05 16:17:17 +0800

    IEATTA.

--- src/lib/photobrowser.js
+++ src/lib/photobrowser.js
@@ -1,17 +1,30 @@
 'use strict'
 
+import {adjustRouterQuery} from '../lib/link'
+
 export default class PhotoBrowser {
   onPreIconClick() {
-    const {photosListTask, selectPhotoIndex} = this.state;
-    const photos = photosListTask.results;
+    const {selectPhotoIndex} = this.state;
 
     let preIndex = selectPhotoIndex - 1;
     if (preIndex < 0) preIndex = 0;
+
     this.setState({selectPhotoIndex: preIndex})
 
+    this._pushNewRouter(preIndex)
+  }
+
+  _pushNewRouter(index) {
+    const {photosListTask} = this.state;
+    const photos = photosListTask.results;
+
+    const pathname = this.props.location.pathname;
+    const lastQuery = this.props.location.query;
+    const newQuery = {select: photos[index].id};
+
     this.props.router.push({
-      pathname: this.props.location.pathname,
-      query: {select: photos[preIndex].id}
+      pathname,
+      query: Object.assign({}, lastQuery, newQuery)
     })
   }
 
@@ -24,10 +37,7 @@ export default class PhotoBrowser {
     if (nextIndex >= totalPhotosLength) nextIndex = totalPhotosLength - 1;
     this.setState({selectPhotoIndex: nextIndex})
 
-    this.props.router.push({
-      pathname: this.props.location.pathname,
-      query: {select: photos[nextIndex].id}
-    })
+    this._pushNewRouter(nextIndex)
   }
 
 
