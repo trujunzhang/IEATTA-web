55ea90690b502fbff89e560848a7864cf1dd035f
trujunzhang <trujunzhang@gmail.com>
2017-09-09 16:24:03 +0800

    IEATTA.

--- src/lib/appConstants.js
+++ src/lib/appConstants.js
@@ -12,6 +12,7 @@ const {
   PARSE_PEOPLE_IN_EVENTS
 } = require('./constants').default
 
+const UUID = require('../components/vendor/uuid');
 
 const AppConstants = {}
 
@@ -42,6 +43,96 @@ AppConstants.SubDomainPhotos = {
   PARSE_RECIPES: 'recipe_photos',
   PARSE_USERS: 'user_local_photos',
 }
+
+
+AppConstants.generateNewRestaurantRealmObject = function () {
+  return {
+    objectId: UUID.create().toString(),
+    uniqueId: UUID.create().toString(),
+    listPhotoId: ''
+  }
+}
+
+AppConstants.generateNewEventRealmObject = function (restaurant) {
+  return {
+    objectId: UUID.create().toString(),
+    uniqueId: UUID.create().toString(),
+    start: new Date(),
+    end: new Date(),
+    restaurant: {
+      id: restaurant.objectId,
+      uniqueId: restaurant.uniqueId
+    },
+    // For test
+    displayName: 'e001',
+    want: 'first Event'
+  }
+}
+
+AppConstants.getRelativeModel = function (modelType, objectSchemaName, modelSchema) {
+  const currentPhotoType = AppConstants.realmTypes[objectSchemaName]
+
+  return (currentPhotoType === modelType) ?
+    {
+      id: modelSchema.objectId,
+      uniqueId: modelSchema.uniqueId
+    } :
+    {
+      id: '',
+      uniqueId: ''
+    };
+
+}
+
+AppConstants.generateNewRealmPhotoObject = function ({modelType, model}) {
+  return {
+    objectId: UUID.create().toString(),
+    uniqueId: UUID.create().toString(),
+    photoType: modelType,
+    // Pointer
+    restaurant: AppConstants.getRelativeModel(modelType, PARSE_RESTAURANTS, model),
+    recipe: AppConstants.getRelativeModel(modelType, PARSE_RECIPES, model),
+    user: AppConstants.getRelativeModel(modelType, PARSE_USERS, model),
+
+  }
+}
+
+AppConstants.generateRelativeObjects = function (forItem, reviewType) {
+  const defaultRelativeObject =
+    {
+      restaurant: {id: '', uniqueId: ''},
+      event: {id: '', uniqueId: ''},
+      recipe: {id: '', uniqueId: ''}
+    };
+  defaultRelativeObject[reviewType] =
+    {
+      id: forItem.objectId,
+      uniqueId: forItem.uniqueId
+    }
+  return defaultRelativeObject;
+}
+
+AppConstants.generateEditReviewObject = function ({user, forItem, objectSchemaName}, reviewRating = 0, lastModel = {}) {
+  const reviewType = AppConstants.realmTypes[objectSchemaName]
+  const relativeObject = AppConstants.generateRelativeObjects(forItem, reviewType);
+  debugger
+  return {
+    objectId: lastModel.objectId || UUID.create().toString(),
+    uniqueId: lastModel.uniqueId || UUID.create().toString(),
+    // Attributes
+    rating: reviewRating,
+    body: lastModel.body || '',
+    reviewType: reviewType,
+    // Pointer
+    user: {
+      id: user.id || '',
+      uniqueId: user.uniqueId || ''
+    },
+    // Relation
+    ...relativeObject,
+  }
+}
+
 
 export default AppConstants;
 
