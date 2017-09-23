f7a78032ab0125b60ca8a104fe3dd9a463c4c74f
trujunzhang <trujunzhang@gmail.com>
2017-09-23 09:30:55 +0800

    IEATTA.

--- src/components/lib/photos/photosbrowser/F8PhotosLoggedUserTitleHeader.jsx
+++ src/components/lib/photos/photosbrowser/F8PhotosLoggedUserTitleHeader.jsx
@@ -9,6 +9,9 @@ import {
 
 
 class F8PhotosLoggedUserTitleHeader extends Component {
+  onEditPhotosPress() {
+
+  }
 
   render() {
     const {forObject, modelType, userProfile, photosListTask} = this.props;
@@ -43,11 +46,24 @@ class F8PhotosLoggedUserTitleHeader extends Component {
               </h1>
             </div>
 
+            {/*{this.renderEditButtons()}*/}
 
-            <div className="arrange_unit nowrap media-header_actions">
+          </div>
 
-              <a
-                className="ybtn ybtn--primary u-space-r1">
+        </div>
+
+      </div>
+
+    )
+  }
+
+  renderEditButtons() {
+    return (
+      <div className="arrange_unit nowrap media-header_actions">
+
+        <a
+          onClick={this.onEditPhotosPress.bind(this)}
+          className="ybtn ybtn--blue u-space-r1">
                <span id="icon_24X24"
                      className="icon icon--24-add-photo icon--size-24 icon--currentColor u-space-r1 icon--fallback-inverted">
                      <svg className="icon_svg">
@@ -55,15 +71,8 @@ class F8PhotosLoggedUserTitleHeader extends Component {
                          d="M5 7V5a1 1 0 0 1 1-1h4V3a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1v1h4a1 1 0 0 1 1 1v2H5zm13 12a3 3 0 0 1-3 3H9a3 3 0 0 1-3-3V8h12v11zm-8-8H9v8h1v-8zm5 0h-1v8h1v-8z"/>
                      </svg>
                </span>
-                {"Edit Photos"}
-              </a>
-
-            </div>
-
-
-          </div>
-
-        </div>
+          {"Edit Photos"}
+        </a>
 
       </div>
 
