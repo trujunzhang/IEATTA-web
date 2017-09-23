a925d7f69b3d6d70a89936a9466d027f007e0aad
trujunzhang <trujunzhang@gmail.com>
2017-09-23 18:14:59 +0800

    IEATTA.

--- src/components/lib/photos/photosbrowser/F8PhotosCollectionView.jsx
+++ src/components/lib/photos/photosbrowser/F8PhotosCollectionView.jsx
@@ -13,10 +13,14 @@ const {
 } = require('../../../../lib/constants').default
 
 class F8PhotosCollectionView extends Component {
+  onTrashIconPress = () => {
+
+  }
 
   renderLeftTopTrash() {
     return (
       <button className="chiclet-link u-cursor-pointer show-tooltip js-delete-review-draft"
+              onClick={this.onTrashIconPress}
               id="photos-browser-cell-item-button-trash">
                 <span id="icon_18X18" className="icon icon--18-trash icon--size-18 icon--currentColor">
                      <svg className="icon_svg">
@@ -24,7 +28,7 @@ class F8PhotosCollectionView extends Component {
                               d="M3 5V3h4V2h4v1h4v2H3zm11 9c0 1.1-.9 2-2 2H6c-1.1 0-2-.9-2-2V6h10v8zM8 8.5a.5.5 0 0 0-.5-.5.5.5 0 0 0-.5.5v5a.5.5 0 0 0 .5.5.5.5 0 0 0 .5-.5v-5zm3 0a.5.5 0 0 0-.5-.5.5.5 0 0 0-.5.5v5a.5.5 0 0 0 .5.5.5.5 0 0 0 .5-.5v-5z"/></svg>
                 </span>
         <span className="tooltip-wrapper">
-                    <span className="tooltip">Delete draft</span>
+                    <span className="tooltip">Delete photo</span>
                 </span>
       </button>
     )
@@ -45,7 +49,7 @@ class F8PhotosCollectionView extends Component {
               src={Photos.getThumbnailUrl(photo)}/>
           </Link>
 
-          {this.renderLeftTopTrash()}
+          {photoTitleType === PHOTO_BROWSER_LOGGED_USER_TITLE && this.renderLeftTopTrash()}
 
           {photoTitleType === PHOTO_BROWSER_LOGGED_USER_TITLE && this.renderOverLayForUserProfile(photoInfo, photo, index)}
           {photoTitleType === PHOTO_BROWSER_NORMAL_TITLE && this.renderOverLay(photoInfo)}
@@ -79,26 +83,20 @@ class F8PhotosCollectionView extends Component {
                   width="30"
                   height="30"
                   placeholderSource={"/default/user_30_square.png"}
-                  source={userImageUrl}
-                />
-
+                  source={userImageUrl}/>
               </Link>
-
             </div>
-
-
           </div>
 
           <div className="media-story" id="photos-browser">
 
-            <span className="author">
-                {"by"}
-              <Link className="user-display-name js-analytics-click margin-left-4"
-                    {...linkProperty}
-                    id="dropdown_user-name">
-                    {userName}
-              </Link>
-                </span>
+          <span className="author">{"by"}
+            <Link className="user-display-name js-analytics-click margin-left-4"
+                  {...linkProperty}
+                  id="dropdown_user-name">
+          {userName}
+          </Link>
+          </span>
           </div>
 
         </div>
