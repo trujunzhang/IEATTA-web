08f65cbd264df8e50e70b0231276f397710bbf2d
trujunzhang <trujunzhang@gmail.com>
2017-09-24 17:13:44 +0800

    IEATTA.

--- src/parse/recorderUtiles.js
+++ src/parse/recorderUtiles.js
@@ -56,10 +56,21 @@ async function updateParseRecorder(objectSchemaName, parseInstance) {
   // After saved recorder, the 'updatedAt' column will be updated automatically.
   // So that new 'updatedAt' will notify the mobile app to update their local database.
   await recorder.save()
+}
 
+async function updateParseRecorderFlagStatus(objectSchemaName, parseInstance) {
+  const recordType = AppConstants.realmTypes[objectSchemaName]
+  let recorder = await getQueryByType(PARSE_RECORDS).equalTo(recordType, parseInstance).first()
+
+  if (!!recorder) {// Exist, set 'flag' to new value.
+
+  } else {
+    throw new Error(`The recorder that recorded the ${objectSchemaName} had been removed!`)
+  }
 }
 
 export default {
   // Update the model's record after saved it.
   updateParseRecorder,
+  updateParseRecorderFlagStatus,
 }
