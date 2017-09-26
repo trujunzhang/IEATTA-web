3c23d586e9b1b8041d346dff9ca7d611afc13464
trujunzhang <trujunzhang@gmail.com>
2017-09-26 11:01:04 +0800

    IEATTA.

--- src/components/lib/photos/photosbrowser/F8PhotosCollectionItemView.jsx
+++ src/components/lib/photos/photosbrowser/F8PhotosCollectionItemView.jsx
@@ -61,6 +61,62 @@ class F8PhotosCollectionItemView extends Component {
     )
   }
 
+
+  /**
+   * Render default overlay.
+   *
+   * @param photoInfo
+   * @returns {XML}
+   */
+  renderOverLay(photoInfo) {
+    const {overlay} = photoInfo;
+    const {user} = overlay;
+    const userName = user.username;
+    const userImageUrl = user.imageUrl;
+    const userLink = user.userProfileUrl;
+    const linkProperty = !!userLink ? {to: userLink} : {};
+
+    return (
+      <div className="photo-box-overlay js-overlay">
+        <div className="media-block photo-box-overlay_caption">
+          <div className="media-avatar avatar">
+            <div className="photo-box pb-30s">
+
+              <Link {...linkProperty} className="js-analytics-click">
+
+                <Telescope.components.F8PlaceHolderImage
+                  alt={userName}
+                  width="30"
+                  height="30"
+                  placeholderSource={"/default/user_30_square.png"}
+                  source={userImageUrl}
+                />
+
+              </Link>
+
+            </div>
+
+
+          </div>
+
+          <div className="media-story" id="photos-browser">
+
+            <span className="author">
+                {"by"}
+              <Link className="user-display-name js-analytics-click margin-left-4"
+                    {...linkProperty}
+                    id="dropdown_user-name">
+                    {userName}
+              </Link>
+                </span>
+          </div>
+
+        </div>
+      </div>
+
+    )
+  }
+
   render() {
     const {photoInfo, photo, index} = this.props;
     const {forObject, modelType, photoTitleType} = this.props;
